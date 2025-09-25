c53dc9d788ea9c84040621cd36f00176111026b6
mateus artem silva <mateus@lumahealth.io>
2025-09-25 12:51:33 -0300

    fix(node-sftp-server): remove _sshstream reference #86b6ujqnf

--- node-sftp-server.js
+++ node-sftp-server.js
@@ -147,7 +147,6 @@ var SFTPServer = (function(superClass) {
         return self.emit("end");
       });
       return client.on('ready', function(channel) {
-        client._sshstream.debug = debug;
         return client.on('session', function(accept, reject) {
           var session;
           session = accept();
